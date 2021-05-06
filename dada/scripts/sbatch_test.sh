b6ddab5e0b5e863b699795312ff5ed4e5244c573
Rohit Kaushik <kaushikrohit325@gmail.com>
2021-05-06 15:30:38 +0200

    add scripts for training on server

---
+++ dada/scripts/sbatch_test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#SBATCH  --output=log/%j.out
+#SBATCH  --gres=gpu:1
+#SBATCH  --mem=30G
+
+source /scratch_net/biwidl217/rokaushik/conda/bin/activate
+conda activate pytcu10
+python test.py --cfg ./configs_s2c/dada_test.yml
